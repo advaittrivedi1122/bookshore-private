29af5645d4aa0a6413be9e7edb8017822630dcdd
Advait Trivedi <trivediadvait0@gmail.com>
2024-02-28 02:45:57 +0530

    basic file upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,64 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { FirebaseStorage, StorageReference, UploadResult, getBlob, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import firebaseApp from './firebase/app'
+import axios from 'axios'
 
 function App() {
-  const [value, setValue]  = useState("")
+  const [value, setValue]: any  = useState(null)
+  const [uploaded, setUploaded] = useState(false)
+  const downloadUrl: string = "https://firebasestorage.googleapis.com/v0/b/bookshore-d046a.appspot.com/o/images%2Fastronaut.jpg?alt=media&token=9b0ad37d-b40f-4cb7-b0be-f554d1a1b84c"
+
+  const handleChange = (event: any) => {
+    // event.preventDefault()
+    console.log(event.target.files[0])
+    setValue(event.target.files[0])
+  }
+
+  const handleClick = (url: string, filename: string) => {
+    fetch(url, {
+      method : "GET",
+      headers : {
+        "Access-Control-Allow-Origin" : "*"
+      }
+    })
+    .then((res) => {
+      console.log("🚀 ~ .then ~ axios res:", res)
+    })
+  }
+
+  const uploadFile = async (): Promise<void> => {
+    const storage: FirebaseStorage = getStorage(firebaseApp)
+    const storageRef: StorageReference = ref(storage, "images/" + value.name)
+    let res: UploadResult = await uploadBytes(storageRef, value)
+    console.log("🚀 ~ uploadFile ~ res:", res)
+    const url = await getDownloadURL(res.ref)
+    console.log("🚀 ~ uploadFile ~ url:", url)
+    const fileBlob = await getBlob(res.ref)
+    console.log("🚀 ~ uploadFile ~ fileBlob:", fileBlob)
+    setUploaded(true)
+  }
 
   useEffect(()=>{
-    const getResponse = async ()=>{
-      console.log("function called")
-      let response: Response | string = await fetch("http://localhost:8080")
-      response = await response.text()
-      console.log("🚀 ~ getResponse ~ response:", response)
-      setValue(response)
-    }
-    if(!value){
-      getResponse()
+    console.log("value updated")
+    if(!uploaded && value) {
+      // uploadFile()
     }
   }, [value])
 
   return (
     <>
-      <h1>{value ? value : "Hello"}</h1>
+    <h1 style={{color:'red'}}>
+    {value ? value.name : "null"}
+    </h1>
+    {/* <form onSubmit={handleSubmit}> */}
+      <input type="file" name="pdf" id="pdf" onChange={handleChange} />
+      {/* <a href="https://firebasestorage.googleapis.com/v0/b/bookshore-d046a.appspot.com/o/images%2Fastronaut.jpg?alt=media&token=9b0ad37d-b40f-4cb7-b0be-f554d1a1b84c" download={"newImage.jpg"} target='_blank'> */}
+        <button onClick={()=>{handleClick(downloadUrl, "newImage.jpg")}}>
+        Download
+        </button>
+      {/* </a> */}
+    {/* </form> */}
     </>
   )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const mode: string = "development"
+const env = loadEnv(mode, process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env" : env
+  },
   plugins: [react()],
 })
