62a3c44e2016fd22cf594188f1218a596c6754f7
Advait Trivedi <trivediadvait0@gmail.com>
2024-02-28 20:51:23 +0530

    download image works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { FirebaseStorage, StorageReference, UploadResult, getBlob, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
 import firebaseApp from './firebase/app'
-import axios from 'axios'
+import  { saveAs } from 'file-saver'
 
 function App() {
   const [value, setValue]: any  = useState(null)
@@ -16,15 +16,17 @@ function App() {
   }
 
   const handleClick = (url: string, filename: string) => {
-    fetch(url, {
-      method : "GET",
-      headers : {
-        "Access-Control-Allow-Origin" : "*"
-      }
-    })
-    .then((res) => {
-      console.log("🚀 ~ .then ~ axios res:", res)
-    })
+    saveAs(url, filename)
+    // fetch(url, {
+    //   method : "GET",
+    //   headers : {
+    //     "Access-Control-Allow-Origin" : "*"
+    //   }
+    // })
+    // .then((res) => {
+    //   console.log("🚀 ~ .then ~ axios res:", res)
+    // })
+    
   }
 
   const uploadFile = async (): Promise<void> => {
@@ -36,13 +38,24 @@ function App() {
     console.log("🚀 ~ uploadFile ~ url:", url)
     const fileBlob = await getBlob(res.ref)
     console.log("🚀 ~ uploadFile ~ fileBlob:", fileBlob)
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    // a.style = "display: none";
+    let fileDownloadUrl = window.URL.createObjectURL(fileBlob);
+    console.log("🚀 ~ uploadFile ~ fileDownloadUrl:", fileDownloadUrl)
+    a.href = fileDownloadUrl;
+    a.download = "image.jpg";
+    a.click();
+    window.URL.revokeObjectURL(url);
     setUploaded(true)
   }
+
+  
 
   useEffect(()=>{
     console.log("value updated")
     if(!uploaded && value) {
-      // uploadFile()
+      uploadFile()
     }
   }, [value])
 
@@ -53,8 +66,10 @@ function App() {
     </h1>
     {/* <form onSubmit={handleSubmit}> */}
       <input type="file" name="pdf" id="pdf" onChange={handleChange} />
-      {/* <a href="https://firebasestorage.googleapis.com/v0/b/bookshore-d046a.appspot.com/o/images%2Fastronaut.jpg?alt=media&token=9b0ad37d-b40f-4cb7-b0be-f554d1a1b84c" download={"newImage.jpg"} target='_blank'> */}
-        <button onClick={()=>{handleClick(downloadUrl, "newImage.jpg")}}>
+      {/* <a href="blob:http://localhost:5173/2822c54e-5dc9-4f8a-b812-d1e241699141" download={"newImage.jpg"}> */}
+        <button onClick={()=>{
+          handleClick("blob:http://localhost:5173/3101d4b1-a65d-416e-91e1-f4a7bef1df32", "newImage.jpg")
+        }}>
         Download
         </button>
       {/* </a> */}
